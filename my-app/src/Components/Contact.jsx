682dc7c55a3c35b62e791e8652575eb42dae3335
amira <amirabourogaa0@gmail.com>
2020-10-16 05:32:35 -0700

    section sign up created

--- my-app/src/Components/Contact.jsx
+++ my-app/src/Components/Contact.jsx
@@ -1,12 +1,43 @@
-import React, { Component } from 'react'
-
-export default class Contact extends Component {
-    render() {
-        return (
-            <div>
-               <h1> Contact us</h1> 
-               
-            </div>
-        )
+import React, { Component } from "react";
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+class Contact extends Component {
+  render() {
+    
+    const containerStyle = {
+      position: 'relative',  
+      width: '100%',
+      height: '100%'
     }
+    return (
+      <div>
+        <h1> Contact us</h1>
+        <p>
+          <h2> Adress: Ariana Tunis </h2>
+          <h2> Headquarter: Djerba - Zarzis</h2>
+          <h2> Phone : +21658202884</h2>
+          <h2> Phone : +21654734755</h2>
+          <h2> Email : amirabourogaa0@gmail.com </h2>
+        </p>
+        <Map 
+           google={this.props.google} zoom={14}
+           initialCenter={{
+            lat: 36.94592,
+            lng: 10.1711872
+          }}
+            >
+              
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+       
+        </InfoWindow>
+      </Map>
+      </div>
+    );
+  }
 }
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyCABJNvq7gj89EsfbRhnze6BaiD5igoF6k")
+})(Contact)
