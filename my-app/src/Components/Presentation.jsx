c6a34520fb118822c4ec09c047ba10a596603d0e
amira <amirabourogaa0@gmail.com>
2020-10-16 15:03:40 -0700

    design finished

--- my-app/src/Components/Presentation.jsx
+++ my-app/src/Components/Presentation.jsx
@@ -4,9 +4,11 @@ export default class Presentation extends Component {
   render() {
     return (
       <div >
-        
+        <span style={{position:"flex",marginTop:'100px'}}>
+      <iframe style={{marginTop:"100px"}}width="950" height="600" src="https://www.youtube.com/embed/9HP8qZzZYh4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+       <div style={{backgroundColor:"black",width:'50%'}}>
         <h1> About us</h1>
-        <h3>
+        <h3 style={{color:"white"}}>
           {" "}
           AK is a sanitation company,<br></br>
           created in 2020 by a team of high-level technicians with extensive
@@ -15,7 +17,8 @@ export default class Presentation extends Component {
           <br></br> in accordance with international standards to intervene in
           dangerous sites{" "}
         </h3>
-        
+        </div>
+        </span>
       </div>
     );
   }
