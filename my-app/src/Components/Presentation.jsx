decc94d512daa098145871e388f9295ddb786234
amira <amirabourogaa0@gmail.com>
2020-10-16 10:10:44 -0700

    almost finish still some style changes and dataBase changes

--- my-app/src/Components/Presentation.jsx
+++ my-app/src/Components/Presentation.jsx
@@ -1,17 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Presentation extends Component {
-    render() {
-        return (
-            <div>
-                <h1> About us</h1>
-               <h3> AK is a sanitation company,<br></br>
+  render() {
+    return (
+      <div >
+        
+        <h1> About us</h1>
+        <h3>
+          {" "}
+          AK is a sanitation company,<br></br>
           created in 2020 by a team of high-level technicians with extensive
           experience <br></br>
           and having the material resources
           <br></br> in accordance with international standards to intervene in
-          dangerous sites  </h3>
-            </div>
-        )
-    }
+          dangerous sites{" "}
+        </h3>
+        
+      </div>
+    );
+  }
 }
